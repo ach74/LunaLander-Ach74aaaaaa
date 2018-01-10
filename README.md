1f0e34bb90e964a76cf1c502841aad955ab107c2
ach74 <32370243+ach74@users.noreply.github.com>
2018-01-11 00:17:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,112 @@
+# Lunar lander con html, css y javascript
+
+Juego terminado: https://cdn.rawgit.com/ach74/LunaLander-Ach74/422649b3/index.html
+
+**Autor:** Achraf El Ouakili
+
+**Asignatura:** Lenguaje de marcas
+
+## PROYECTO:
+
+El objetivo final de este proyecto es desarrollar un juego, en que una nave tiene que aterrizar en la luna, este mismo sera creado estéticamente con HTML y CSS. El desplazamiento de la nave y las funciones de los paneles de control estarán definidos en el JavaScript.
+El proyecto que he decidido desarrollar ha sido el que diseñe al principio de este proyecto, pero añadiendo algunas modificaciones.
+
+Este es el resultado final para la versión de ordenador:
+![Imagen](img/screenshots/1.PNG)
+
+Y este es el resultado final para la versión para móviles:
+![Imagen](img/screenshots/2.PNG)
+
+## Explicación:
+
+### Botones de la versión para ordenador:
+
+Como se puede observar en la versión para ordenadores tenemos una serie de opciones para interactuar con el juego:
+![Imagen](img/screenshots/3.PNG)
+
+* El primer botón situado más a la izquierda, es el de sonido. Una vez pulsado sonara una melodía que nos acompañara mientras jugamos. Por defecto el juego viene silenciado. Cuando pulsemos este boton tambien cambiara. Y pasaremos a tener otro que nos permitira silenciar el juego.
+Los botones que irán intercambiando entre si son los siguientes:
+
+![Imagen](img/Sonido.png)
+![Imagen](img/NoSonido.png)
+
+* El siguiente es el de reinicio, con el podremos volver a empezar cuando queramos.
+
+* A continuación viene el botón encargado de pausar el juego o ponerlo en marcha. Este botón ira alternando entre los dos estado según lo pulsemos. Por defecto viene en pausa.
+
+![Imagen](img/Pause.png)
+![Imagen](img/Play.png)
+
+* El botón que veremos a continuación es el botón de "Ayuda" que nos abrirá una pestaña donde nos explicara el juego. Panel que abrirá es el siguiente:
+
+![Imagen](img/screenshots/4.png)
+
+Como podemos observar hay un botón el de "About" si le damos nos llevara a otra pagina donde nos hablaran un poco más del juego.
+
+* Por ultimo tenemos el botón de "Ajustes", el cual una vez desplegado nos mostrara las siguientes funciones:
+
+
+![Imagen](img/screenshots/5.PNG)
+
+Como se muestra podremos cambiar la dificultad y la nave solo con hacer click. Al cambiar la nave, lo que se hace es ocultar la imagen de la nave que sé está mostrando, para poner en visible la que está oculta. Y así sucesivamente. Las naves con las cuales podremos jugar son las siguientes:
+
+![Imagen](img/Covete.png)
+![Imagen](img/Nave.png)
+
+Si lo que queremos es cambiar la dificultad del juego, solo con elegir una de las opciones que se nos muestra bastara. Dependiendo el nivel de dificultad que elijamos el valor que influye sobre la gravedad aumentara o disminuirá.
+
+### Botones de la versión para móvil:
+
+Para la versión móvil tenemos menos botones en pantalla, un total de 2:
+
+![Imagen](img/screenshots/6.PNG)
+
+* El botón rojo, es el encargado de recoger todos los demás botones que teníamos en la versión para ordenador. Una vez le demos se nos mostraran la mismas opciones que tenemos para ordenador pero recogidas en un recuadro que se mantiene oculto cuando jugamos. Ademas de queuna ves le demos el juego de pausara.
+
+El panel que se nos va a desplegás es el siguiente:
+
+
+![Imagen](img/screenshots/7.PNG)
+
+Como podemos observar tenemos las mismas opciones que teníamos en la versión de escritorio, con lo cual hacen las mismas funciones.
+
+* El botón verde (Pause) hace lo mismo que en la versión de escritorio. Controlar si el juego para detenerlo o no.
+
+### Indicadores:
+
+En las dos versiones tenemos los mismos indicadores solo que en la versión para móviles, la anchura se hace un poco más pequeña. Los indicadores son los siguientes :
+
+![Imagen](img/screenshots/8.PNG)
+
+* El indicador de velocidad nos marcara a que velocidad estamos cayendo, o por su contrario ascendiendo.
+
+* El indicador de altura nos marcara cuenta distancia estamos cayendo.
+
+* El siguiente es el indicador de fuel, que mediante una barra vertical nos mostrara cuanta gasolina nos queda en el tanque de gasolina.
+
+### Otros elementos:
+
+* Cuando interactuemos con algún elemento que nos vaya a mostrar información o más opciones, como por ejemplo la opción de "Ayuda", tenemos un botón situado en la parte superior izquierda, que nos permite volver atrás, o más bien hacer que desaparezca el elemento que hemos abierto.
+
+El botón es el siguiente:
+
+![Imagen](img/x.png)
+
+* Otro botón muy importante es el botón para propulsar la nave, en este caso tiene aspecto de palanca, y solo propulsa la nave cuando estos pulsando en él, este botón se mantiene automáticamente apagado cuando no es usado o cuando el juego ha finalizado. Ademas de que cambia de aspecto según lo pulsemos o no.
+
+Aquí le muestro sus dos estados:
+Botón sin ser pulsado:
+
+![Imagen](img/BotonOFF.png)
+
+Botón pulsado:
+
+![Imagen](img/BotonON.png)
+
+## Inicio del juego
+
+Una vez iniciemos el juego la nave ira descendiendo dependiendo de la dificultad elegida. Y para controlar dicho descenso tenemos el botón de propulsión que cada ver que lo pulsemos la imagen de la nave cambiara a un "GIF", que nos dará la impresión de que esta desprendiendo fuego. Dichos "GIF" son los siguientes:
+
+![Imagen](img/Covete2.GIF)
+![Imagen](img/Nave2.gif)
+
---
+++ js/js.js
@@ -0,0 +1,379 @@
+//ENTORNO
+var g = 1.622;
+var dt = 0.016683;
+var timer=null;
+var timerFuel=null;
+//NAVE
+var y = 10; // altura inicial y0=10%, debe leerse al iniciar si queremos que tenga alturas diferentes dependiendo del dispositivo
+var v = 0;
+var c = 100;
+var a = g; //la aceleración cambia cuando se enciende el motor de a=g a a=-g (simplificado)
+//MARCADORES
+var velocidad = null;
+var altura = null;
+var combustible = null;
+var singasofa = false;
+//Al cargar por completo la página...
+var siempreV = null;
+var siempreA = null;
+//Cambio de nave
+var NaveCambio = 1;
+
+
+window.onload = function InicioJuego(){
+
+
+	velocidad = document.getElementById("velocidad");
+	altura = document.getElementById("altura");
+	combustible = document.getElementById("gastofuel");
+
+	//encender/apagar el motor al hacer click en la pantalla
+
+
+	/*encender/apagar al apretar/soltar una tecla
+	* document.onkeydown = motorOn;
+	*document.onkeyup = motorOff;
+	*/
+	//Empezar a mover la nave justo después de cargar la página
+	stop();
+
+	//VALIDAR BOTONES INICIAR EL JUEGO
+
+	document.getElementById("BotonPlay").onclick=function(){play();};
+	document.getElementById("BotonPause").onclick=function(){pause();};
+	document.getElementById('BotonReiniciar').onclick=function(){Restart();};
+	//Sonido
+	document.getElementById('BotonSonido').onclick=function(){sonido();};
+	document.getElementById('BotonSonidoNo').onclick=function(){nosonido();};
+
+	document.getElementById("BotonAyuda").onclick=function(){help();};
+	document.getElementById("CerrarAyuda").onclick=function(){closeHelp();};
+
+	document.getElementById("BotonAjustes").onclick=function(){settings();};
+	document.getElementById("CerrarAjustes").onclick=function(){Closesettings();};
+
+
+
+	
+	//Niveles
+	document.getElementById("Facil").onclick=function(){facil();};
+	document.getElementById("Normal").onclick=function(){normal();};
+	document.getElementById("Difícil").onclick=function(){dificil();};
+	document.getElementById("Imposible").onclick=function(){imposible();};
+
+
+
+	//Volver a jugar
+	document.getElementById("VolverAjugar").onclick=function(){Restart();};
+
+	//Desplegar menu
+	document.getElementById("BotonMenu").onclick=function(){menu();};
+	document.getElementById('cerrarMenu').onclick=function(){cerrartodo();};
+
+
+
+	//Botones movil
+	document.getElementById('S-Replay').onclick=function(){Restart();};
+	document.getElementById('S-Setting').onclick=function(){
+		settings();
+		closemenu();
+	};
+	document.getElementById('S-Help').onclick=function(){
+		help();
+		closemenu();
+	};
+
+	document.getElementById('S-Sound').onclick=function(){Sonido_S();};
+	document.getElementById('NS-Sound').onclick=function(){NSonido_S();};
+	//Game Over
+	document.getElementById('VolverAJugar').onclick=function(){Restart();};
+
+	//Ir a about
+	document.getElementById('Abutton').onclick=function(){direccion();};
+
+
+	//Cambiar naves
+	document.getElementById("CambiarNave").onclick=function(){otranave();};
+}
+
+function direccion(){
+location.href='About.html';
+}
+
+function Sonido_S(){
+		document.getElementById('demo').play();
+		document.getElementById('S-Sound').style.display="none";
+		document.getElementById('NS-Sound').style.display="block";
+
+}
+function NSonido_S(){
+		document.getElementById('demo').pause();
+		document.getElementById('NS-Sound').style.display="none";
+		document.getElementById('S-Sound').style.display="block";
+
+}
+
+
+//Mostrar menu
+function menu(){
+	document.getElementById("SmartphoneMenu").style.display="block";
+	closeHelp();
+	Closesettings();
+	pause();
+}
+function closemenu(){
+	document.getElementById("SmartphoneMenu").style.display="none";
+}
+
+
+//Cambiar nave
+function otranave(){
+	if (NaveCambio==1){
+		document.getElementById("imgNave").style.display="none";
+		document.getElementById("imgCohete").style.display="block";
+		NaveCambio=2;
+	}
+	else if (NaveCambio==2){
+		document.getElementById("imgCohete").style.display="none";
+		document.getElementById("imgNave").style.display="block";
+		NaveCambio=1;
+	}
+}
+
+
+
+
+//Niveles de dificultat
+function facil(){
+	g=g;
+	reset();
+	cerrartodo();
+	play();
+}
+function normal(){
+	g=10;
+	reset();
+	cerrartodo();
+	play();
+}
+function dificil(){
+	g=100;
+	reset();
+	cerrartodo();
+	play();
+}
+function imposible(){
+	g=1000;
+	reset();
+	cerrartodo();
+	play();
+}
+
+function cerrartodo(){
+	Closesettings();
+	closeHelp();
+	document.getElementById("loser").style.display="none";	
+	document.getElementById("winner").style.display="none";
+	document.getElementById("SmartphoneMenu").style.display="none";
+}
+
+
+//Botones
+function BotonOn(){
+		document.getElementById('BotonOn').style.display="none";
+		document.getElementById('BotonOff').style.display="inline-block";
+  		motorOn();
+}
+function BotonOff(){
+		document.getElementById('BotonOn').style.display="inline-block";
+		document.getElementById('BotonOff').style.display="none";
+		motorOff();
+}
+
+//Definición de funciones
+function start(){
+	//cada intervalo de tiempo mueve la nave
+	timer=setInterval(function(){ moverNave(); }, dt*1000);
+
+
+}
+
+
+
+function stop(){
+	clearInterval(timer);
+	BotonOff();
+}
+
+function moverNave(){
+	//cambiar velocidad y posicion
+	v +=a*dt;
+	y +=v*dt;
+	//actualizar marcadores
+
+	//Velocidad dismepre en positivo
+	if (v > 0) {
+		siempreV = v;
+	}else{
+		siempreV = -v;
+	}
+	//Altura simpre en positivo
+	if (y > 0) {
+		siempreA = y;
+	}else{
+		siempreA =(-1)*y;
+	}
+
+	
+	//mover hasta que top sea un 70% de la pantalla
+	if (y<=72.9){ 
+		document.getElementById("nave").style.top = y+"%"; 
+		velocidad.innerHTML=siempreV.toFixed(2);
+		altura.innerHTML=siempreA.toFixed(2);
+	} else { 
+		stop();
+
+	}
+
+	//Game over
+	 if (siempreA>=72.9) {
+
+	 	if (siempreV<=5) {
+		document.getElementById("winner").style.display="inline-block";
+		}else{
+		document.getElementById("loser").style.display="inline-block";
+		}
+	 }
+
+}
+function motorOn(){
+
+	document.getElementById("imgNave").src="img/Nave2.gif";
+	document.getElementById("imgCohete").src="img/Covete2.GIF";
+
+	//Cambiar estado del boton
+	if (!singasofa){
+			//el motor da aceleración a la nave
+	a=-g;
+	//mientras el motor esté activado gasta combustible
+	if (timerFuel==null)
+	timerFuel=setInterval(function(){ actualizarFuel(); }, 10);
+	
+	}else{
+		motorOff();
+	}
+}
+
+
+function motorOff(){
+	document.getElementById("imgNave").src="img/Nave.png"
+	document.getElementById("imgCohete").src="img/Covete.png"
+	a=g;
+	clearInterval(timerFuel);
+	timerFuel=null;
+}
+
+
+function actualizarFuel(){
+	//Restamos combustible hasta que se agota
+	c-=0.1;
+		if (c == 0 ){
+		c = 0;
+		//combustible.innerHTML=c;	
+		singasofa = true;
+		motorOff(); /**Que se apage el motor cuando el fuel sea 0**/
+		}
+
+		if (c <0 ) {
+			document.getElementById('BotonOn').onmousedown = function () {BotonOff();};
+			document.getElementById('BotonOff').onmouseup= function(){ BotonOff();};
+			motorOff();
+		}
+		combustible.style.height = c +"%";
+
+
+}
+
+// FUNCIONES PARA LOS BOTONES
+
+function sonido(){
+	document.getElementById('BotonSonido').style.display="none";
+	document.getElementById('BotonSonidoNo').style.display="inline-block";
+	document.getElementById('demo').play();
+}
+function nosonido(){
+	document.getElementById('BotonSonido').style.display="inline-block";
+	document.getElementById('BotonSonidoNo').style.display="none";
+	document.getElementById('demo').pause();
+}
+function play(){
+	start();
+	document.getElementById('BotonPause').style.display="inline-block";
+	document.getElementById('BotonPlay').style.display="none";
+	cerrartodo();
+	document.getElementById('BotonOn').onmousedown = function () {BotonOn();};
+	document.getElementById('BotonOff').onmouseup= function(){ BotonOff();};
+}
+function pause(){
+	stop();
+	document.getElementById("BotonPlay").style.display="inline-block";
+	document.getElementById("BotonPause").style.display="none";
+}
+function reset(){
+
+	clearInterval(timer);
+	y = 0;
+	v = 0;
+	c = 100;
+	dt = 0.016683;
+	combustible.style.height = c +"%";
+	document.getElementById("BotonPause").style.display="none";
+	document.getElementById('BotonPlay').style.display="inline-block";
+	BotonOff();
+	cerrartodo();
+}
+
+function Restart(){
+
+	clearInterval(timer);
+		// VOLVER VARIABLE A ESTADO DE INCIO
+	y = 0;
+	v = 0;
+	g = 1.622;
+	c = 100;
+	a = g;
+	dt = 0.016683;
+	combustible.style.height = c +"%";
+	document.getElementById("BotonPause").style.display="none";
+	document.getElementById('BotonPlay').style.display="inline-block";
+	play();
+	BotonOff();
+	cerrartodo();
+	moverNave();
+}
+
+function help(){
+	pause();
+	document.getElementById("Ayuda").style.display="inline-block";
+	Closesettings();
+	BotonOff();
+	document.getElementById("loser").style.display="none";	
+	document.getElementById("winner").style.display="none";
+}
+function closeHelp(){
+	document.getElementById("Ayuda").style.display="none";
+}
+
+function settings(){
+	pause();
+	document.getElementById("ajustes").style.display="inline-block";
+	closeHelp();
+	BotonOff();
+	document.getElementById("loser").style.display="none";	
+	document.getElementById("winner").style.display="none";
+
+}
+function Closesettings(){
+	document.getElementById("ajustes").style.display="none";
+}
+
